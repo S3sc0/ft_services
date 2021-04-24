c9c8b3ea414b6affd72bb6be722342d5cd395841
Ayoub Amzouar <aamzouar@e1r12p4.1337.ma>
2021-04-24 17:45:56 +0000

    this shit won't let us save and load the shitty dashboards

--- grafana/start.sh
+++ grafana/start.sh
@@ -1,3 +1,7 @@
 # this will run grafana-server
 
-/usr/sbin/grafana-server --pidfile=/var/run/grafana-server.pid --config=/etc/grafana.ini --homepath=/usr/share/grafana cfg:default.paths.data=/var/lib/grafana cfg:default.paths.logs=/var/log/grafana cfg:default.paths.plugins=/var/lib/grafana/plugins
+/usr/sbin/grafana-server --pidfile=/var/run/grafana-server.pid --config=/etc/grafana.ini --homepath=/usr/share/grafana cfg:default.paths.data=/var/lib/grafana cfg:default.paths.logs=/var/log/grafana cfg:default.paths.plugins=/var/lib/grafana/plugins </dev/null &>/dev/null &
+
+cp grafana /var/lib
+
+tail -f /dev/null
