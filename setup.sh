9022250d5c22b5ff7b326c2a4558863c245e0655
Ayoub Amzouar <aamzouar@e1r12p4.1337.ma>
2021-05-19 11:46:28 +0100

    moved

--- setup.sh
+++ setup.sh
@@ -1,26 +1,23 @@
-# delete old images
-docker rmi -f engine-x
-docker rmi -f grafana
-docker rmi -f wordpress
-docker rmi -f phpmyadmin
-docker rmi -f mysql
-docker rmi -f influxd
-docker rmi -f ftps
+# the start
+minikube start
+
+eval $(minikube -p minikube docker-env)
 
 # build new images
-docker build -t engine-x nginx
-docker build -t grafana grafana
-docker build -t wordpress wordpress
-docker build -t phpmyadmin phpmyadmin
-docker build -t mysql mysql
-docker build -t influxdb influxdb
-docker build -t ftps ftps
+docker build -t engine-x srcs/nginx
+docker build -t grafana srcs/grafana
+docker build -t wordpress srcs/wordpress
+docker build -t phpmyadmin psrcs/hpmyadmin
+docker build -t mysql srcs/mysql
+docker build -t influxdb srcs/influxdb
+docker build -t ftps srcs/ftps
 
 # build the deployments
-kubectl apply -f nginx/nginx.yaml
-kubectl apply -f grafana/grafana.yaml
-kubectl apply -f wordpress/wordpress.yaml
-kubectl apply -f phpmyadmin/phpmyadmin.yaml
-kubectl apply -f mysql/mysql.yaml
-kubectl apply -f influxdb/influxdb.yaml
-kubectl apply -f ftps/ftps.yaml
+kubectl apply -f srcs/nginx/nginx.yaml
+kubectl apply -f srcs/grafana/grafana.yaml
+kubectl apply -f srcs/wordpress/wordpress.yaml
+kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
+kubectl apply -f srcs/mysql/mysql.yaml
+kubectl apply -f srcs/influxdb/influxdb.yaml
+kubectl apply -f srcs/ftps/ftps.yaml
+kubectl apply -f srcs/configmap.yaml
